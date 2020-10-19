d405ff5aa9a080c2ee557ccd67c9dff549ef30c1
Hofer, Gabriel <gabriel.hofer@mines.sdsmt.edu>
2020-10-19 00:51:05 -0600

    ok

--- cuda.cu
+++ cuda.cu
@@ -5,7 +5,9 @@ Instructor: Dr. Karlsson
 Course: CSC-410 Parallel Computing
 *********************************************************************/
 #include <stdio.h>
+#include <time.h>
 #define THREADS_PER_BLOCK 512
+const int inf = 512;
 
 void printA(int * A, const int n);
 
@@ -16,56 +18,19 @@ __global__ void aux(int * dA, const int n, const int k){
   int index = threadIdx.x + blockIdx.x * blockDim.x;
   if(index > n*n) return;
 	__syncthreads();
-  int i = index / n;
-	int j = index % n;
+  int i = index / n, j = index % n;
   dA[i*n+j] = dA[i*n+j] < (dA[i*n+k]+dA[k*n+j]) ? dA[i*n+j] : dA[i*n+k]+dA[k*n+j];
 }
 
 /*********************************************************************
 Floyd-Warshall Algorithm
 *********************************************************************/
-void floyd(const int n){
-  // size of A in bytes
-  int Asize = n*n*sizeof(int);
-	int inf = 512;
-  // allocate 2D array on Host
-  int * A = (int *)malloc(Asize);
-
-	int tmp[n*n] = { 
-		0, 2, 5, inf, inf, inf, 
-		inf, 0, 7, 1, inf, 8, 
-		inf, inf, 0, 4, inf, inf, 
-		inf, inf, inf, 0, 3, inf, 
-		inf, inf, 2, inf, 0, 3, 
-		inf, 5, inf, 2, 4, 0 };
-
-	memcpy(A,tmp,n*n*sizeof(int));
-
-	// print before
-	printA(A,n);
-
-  // allocate 2D array on Device
-  int * dA=NULL;
-  cudaMalloc((void **)&dA, Asize);
-
-  // copy Array to Device
-  cudaMemcpy(dA, A, Asize, cudaMemcpyHostToDevice);
-
-  for(int k=0;k<n;k++){
-    // call floyd's algorithm
-    aux<<<(n*n+THREADS_PER_BLOCK)/(THREADS_PER_BLOCK),THREADS_PER_BLOCK>>>(dA,n,k);
+void floyd(int * dA, const int n){
+	for(int k=0;k<n;k++){
+		// call floyd's algorithm
+		aux<<<(n*n+THREADS_PER_BLOCK)/(THREADS_PER_BLOCK),THREADS_PER_BLOCK>>>(dA,n,k);
 		cudaDeviceSynchronize();
-  }
-
-  // copy Array back to Host
-  cudaMemcpy(A, dA, Asize, cudaMemcpyDeviceToHost);
-
-	// print result
-	printA(A,n);
-
-  // Cleanup
-  cudaFree(dA);
-  cudaDeviceSynchronize();
+	}
 }
 
 /*********************************************************************
@@ -85,25 +50,9 @@ void printA(int * A, const int n){
 }
 
 /*********************************************************************
-Serial
+Serial, used for checking correctness 
 *********************************************************************/
-void serial(const int n){
-	int Asize = n*n*sizeof(int);
-	int inf = 512;
-  // allocate 2D array on Host
-  int * A = (int *)malloc(Asize);
-
-	int tmp[n*n] = { 
-		0, 2, 5, inf, inf, inf, 
-		inf, 0, 7, 1, inf, 8, 
-		inf, inf, 0, 4, inf, inf, 
-		inf, inf, inf, 0, 3, inf, 
-		inf, inf, 2, inf, 0, 3, 
-		inf, 5, inf, 2, 4, 0 };
-
-	memcpy(A,tmp,n*n*sizeof(int));
-
-	// print before
+void serial(int * A, const int n){
 	printA(A,n);
 	for(int k=0;k<n;k++)
 		for(int i=0;i<n;i++)
@@ -113,18 +62,67 @@ void serial(const int n){
 }
 
 /*********************************************************************
+Usage Statement
+*********************************************************************/
+void Usage(){
+	printf("Usage: ./cuda -N n_integer\n");
+}
+
+/*********************************************************************
+Make Random Matrix 
+*********************************************************************/
+int * makeMatrix(const int n){
+	int * A = (int *)malloc(n*n*sizeof(int));
+	srand(time(0));
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(rand()&1) A[i*n+j]=(rand()%20)+1; // random number in range [1,20]
+			else A[i*n+j]=inf;
+		}
+	}
+	return A;
+}
+
+/*********************************************************************
 Main
 *********************************************************************/
-int main() {
-  int n;
-	printf("size of array: ");
-  cudaDeviceSynchronize();
-  scanf("%d", &n);
-  cudaDeviceSynchronize();
-	printf("%d\n", n);
-  cudaDeviceSynchronize();
+int main(int argc, char *argv[]) {
+	if(argc==1) {
+		Usage();
+		return 0;
+	}
+	// convert argument to integer
+  int n = atoi(argv[2]);
 
-  floyd(n);
+	// allocate memory for graph
+	int * A = makeMatrix(n);
+ 
+	// size of A in bytes
+  int Asize = n*n*sizeof(int);
+
+	// print before
+	printA(A,n);
+
+  // allocate 2D array on Device
+  int * dA=NULL;
+  cudaMalloc((void **)&dA, Asize);
+
+  // copy Array to Device
+  cudaMemcpy(dA, A, Asize, cudaMemcpyHostToDevice);
+
+	// run the algorithm
+  floyd(dA,n);
+
+  // copy Array from Device to Host
+  cudaMemcpy(A, dA, Asize, cudaMemcpyDeviceToHost);
+
+	// print result
+	printA(A,n);
+
+  // Cleanup
+  cudaFree(dA);
+  cudaDeviceSynchronize();
+	
 	//serial(n);
   return 0;
 }
